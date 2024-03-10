d937a413096218e1d8fcefecd5859cf792c32636
Avni <avni0000patel@gmail.com>
2024-03-09 22:06:42 -0500

    change configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb+srv://avni0000patel:database4@cluster0.q0aig2m.mongodb.net/?retryWrites=true&w=majority',
+    process.env.MONGODB_URI || 'mongodb+srv://avni0000patel:crochetuser@cluster0.gqthscx.mongodb.net/?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
